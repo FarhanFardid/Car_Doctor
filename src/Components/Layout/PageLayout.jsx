48c27ddd72cbfa56a63d215ee7a6362e48180900
FarhanFardid <farhanfardid009@gmail.com>
2023-05-14 17:17:23 +0600

    router setup

--- src/Components/Layout/PageLayout.jsx
+++ src/Components/Layout/PageLayout.jsx
@@ -1,9 +1,14 @@
+import { Outlet } from "react-router-dom";
+import Navigataion from "../Shared/Navigataion";
+import Footer from "../Shared/Footer";
 
 
 const PageLayout = () => {
     return (
         <div>
-            
+            <Navigataion></Navigataion>
+            <Outlet></Outlet>
+      <Footer></Footer>
         </div>
     );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,61 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
 
+import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import PageLayout from './Components/Layout/PageLayout.jsx'
+import Home from './Components/Home/Home.jsx'
+import Login from './Components/Pages/Login.jsx'
+import SignUp from './Components/Pages/SignUp.jsx'
+import ServiceDetails from './Components/Services/ServiceDetails.jsx'
+import AddServices from './Components/Services/AddServices.jsx'
+import UpdateServices from './Components/Services/UpdateServices.jsx'
+import ServiceBooking from './Components/Services/ServiceBooking.jsx'
+import ServiceCart from './Components/Services/ServiceCart.jsx'
+
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element:<PageLayout></PageLayout>,
+    children:[
+      {
+        path:'/',
+        element:<Home></Home>,
+
+      },
+      {
+        path:'login',
+        element:<Login></Login>
+      },
+      {
+        path:'signUp',
+        element:<SignUp></SignUp>
+      },
+      {
+        path:'serviceDetails',
+      element:<ServiceDetails></ServiceDetails>
+      },
+      {
+        path:'serviceAdd',
+        element:<AddServices></AddServices>,
+      },
+      {
+        path:'serviceUpdate',
+        element:<UpdateServices></UpdateServices>
+      },
+      {
+        path:'serviceBooking',
+        element:<ServiceBooking></ServiceBooking>
+      },
+      {
+        path:'serviceCart',
+        element:<ServiceCart></ServiceCart>
+      }
+    ]
+  }
+])
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+   <RouterProvider router={router}></RouterProvider>
   </React.StrictMode>,
 )
