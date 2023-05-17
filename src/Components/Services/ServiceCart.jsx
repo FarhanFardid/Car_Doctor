8b3706127d7071e99c41b889301ece83f592e72f
FarhanFardid <farhanfardid009@gmail.com>
2023-05-18 00:03:49 +0600

    all ok

--- src/Components/Services/ServiceCart.jsx
+++ src/Components/Services/ServiceCart.jsx
@@ -58,7 +58,7 @@ const handleConfirm = id =>{
         <div className="hero-overlay bg-opacity-20"></div>
         <div className="text-neutral-content">
           <div className="text-left">
-            <h1 className="mb-5 text-5xl font-bold  text-red-700">Service Cart</h1>
+            <h1 className="mb-5 text-5xl font-bold  text-red-700">Bookings Cart</h1>
             <p className="mb-5 text-white">Enhancing your driving experience with our exceptional car garage service.
 </p>
             <button className="btn bg-slate-500"> Car Service </button>
